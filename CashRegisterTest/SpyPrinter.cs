f8a458c68dcb9598f8c0385dbbd703e2df741ef8
Siqi Song <ssong6@slb.com>
2022-10-26 15:08:37 +0800

    test(example 1): add Should_process_execute_printin test using spyclass

---
+++ CashRegisterTest/SpyPrinter.cs
@@ -0,0 +1,19 @@
+﻿using CashRegister;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CashRegisterTest
+{
+    public class SpyPrinter : Printer
+    {
+        public bool HasPrinted { get; set; }
+        public override void Print(string content)
+        {
+            base.Print(content);
+            HasPrinted = true;
+        }
+    }
+}
